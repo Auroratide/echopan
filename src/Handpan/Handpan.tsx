2a15c60c8ea83d6a1ad8124826b0fe899b5c522c
auroratide <tf.auroratide@gmail.com>
2020-03-07 15:42:37 -0600

    Able to position notes on handpan

--- src/Handpan/Handpan.tsx
+++ src/Handpan/Handpan.tsx
@@ -3,5 +3,13 @@ import * as Styles from './styles'
 import { Note } from './Note'
 
 export const Handpan = () => <Styles.Pan>
-  <Note />
+  <Note angle={0} distance={0.72} />
+  <Note angle={0.125} distance={0.72} />
+  <Note angle={0.25} distance={0.72} />
+  <Note angle={0.375} distance={0.72} />
+  <Note angle={0.5} distance={0.72} />
+  <Note angle={0.625} distance={0.72} />
+  <Note angle={0.75} distance={0.72} />
+  <Note angle={0.875} distance={0.72} />
+  <Note angle={0.5} distance={0} />
 </Styles.Pan>
--- src/Handpan/Note/Note.tsx
+++ src/Handpan/Note/Note.tsx
@@ -1,6 +1,13 @@
 import * as React from 'react'
 import * as Styles from './styles'
 
-export const Note = () => <Styles.Shape>
+interface Props {
+  angle: number    // [0, 1] clockwise
+  distance: number // [0, 1]
+}
 
-</Styles.Shape>
+export const Note = ({ angle, distance }: Props) => <Styles.Shape style={{
+  left: `${0.5 - distance / 2 * Math.cos(angle * Math.PI * 2 + Math.PI / 2)}em`,
+  top: `${0.5 - distance / 2 * Math.sin(angle * Math.PI * 2 + Math.PI / 2)}em`,
+  transform: `translate(-50%, -50%) rotate(${360 * angle}deg)`
+}} />
