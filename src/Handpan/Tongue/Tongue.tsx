2fa92cb02f9f8fc9238b1bfbabad74681e539782
auroratide <tf.auroratide@gmail.com>
2020-03-14 16:16:35 -0500

    Allow tongue to have keyboard shortcuts

--- src/Handpan/Tongue/Tongue.tsx
+++ src/Handpan/Tongue/Tongue.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import * as Styles from './styles'
 import { Note } from '../Note'
+import { Key, Keys } from '../../Keys'
 
 interface Props {
   angle: number    // [0, 1] clockwise
@@ -8,13 +9,23 @@ interface Props {
   rotation: number // [0, 1]
   scale: number
   note: Note
+  shortcut?: Key
+  keys?: Keys
 }
 
-export const Tongue = ({ angle, distance, rotation, scale, note }: Props) => <Styles.Shape style={{
-  left: `${0.5 - distance / 2 * Math.cos(angle * Math.PI * 2 + Math.PI / 2)}em`,
-  top: `${0.5 - distance / 2 * Math.sin(angle * Math.PI * 2 + Math.PI / 2)}em`,
-  transform: `translate(-50%, -50%) rotate(${360 * (angle + rotation)}deg) scale(${scale})`
-}} onClick={note.play} title={note.name()} />
+export const Tongue = ({ angle, distance, rotation, scale, note, shortcut, keys }: Props) => {
+  if(shortcut && keys) {
+    React.useEffect(() => {
+      keys.on(shortcut, note.play)
+    }, [])
+  }
+
+  return <Styles.Shape style={{
+    left: `${0.5 - distance / 2 * Math.cos(angle * Math.PI * 2 + Math.PI / 2)}em`,
+    top: `${0.5 - distance / 2 * Math.sin(angle * Math.PI * 2 + Math.PI / 2)}em`,
+    transform: `translate(-50%, -50%) rotate(${360 * (angle + rotation)}deg) scale(${scale})`
+  }} onClick={note.play} title={note.name()} />
+}
 
 Tongue.defaultProps = {
   rotation: 0,
--- src/Handpan/Tongue/example.tsx
+++ src/Handpan/Tongue/example.tsx
@@ -2,6 +2,7 @@ import * as React from 'react'
 import { render, fireEvent } from '@testing-library/react'
 import { Tongue } from '.'
 import { MockNote } from '../Note'
+import { Key, Keys } from '../../Keys'
 
 describe('Tongue', () => {
   it('plays sound when clicked', () => {
@@ -9,6 +10,18 @@ describe('Tongue', () => {
     const wrapper = render(<Tongue angle={0} distance={0} note={note} />)
 
     fireEvent.click(wrapper.getByTitle('C4'))
+
+    expect(note.timesPlayed).toEqual(1)
+  })
+
+  it('plays sound when shortcut is pressed', () => {
+    const keys = new Keys(document)
+    const note = new MockNote('C4')
+    const wrapper = render(<Tongue angle={0} distance={0} note={note} shortcut={Key.c} keys={keys} />)
+
+    fireEvent.keyDown(wrapper.getByTitle('C4'), {
+      key: 'c'
+    })
 
     expect(note.timesPlayed).toEqual(1)
   })
